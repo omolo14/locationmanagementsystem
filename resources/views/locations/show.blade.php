0d48686cda59cf0e3b42e0f920a2b72ab2ca8314
Boaz <omoloboaz32@gmail.com>
2023-06-13 17:41:01 +0300

    ppp

--- resources/views/locations/show.blade.php
+++ resources/views/locations/show.blade.php
@@ -80,7 +80,6 @@
                         <div class="card">
                             <div class="card-body">
                                 <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
-                                <table id="key-datatable" class="table dt-responsive nowrap w-100">
                                     <thead>
                                         <tr>
                                             <th>#</th>
